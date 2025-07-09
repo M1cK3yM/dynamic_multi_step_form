c05960b4542978eacbf41c588cf1c59abcf8fb58
M1cK3yM <michaelyirdaw652@gmail.com>
2025-07-09 05:01:56 +0300

    add customizable selected and unselected colors

--- lib/src/screen/dynamic_form.dart
+++ lib/src/screen/dynamic_form.dart
@@ -8,7 +8,8 @@ class DynamicForm extends StatefulWidget {
   final Function(
       {int currentIndex,
       Map<String, dynamic>? formSubmitData,
-      Map<String, dynamic>? formInformation,bool? isBack})? currentStepCallBack;
+      Map<String, dynamic>? formInformation,
+      bool? isBack})? currentStepCallBack;
   final GlobalKey<DynamicFormState>? dynamicFormKey;
 
   final Alignment? submitButtonAlignment;
@@ -17,6 +18,8 @@ class DynamicForm extends StatefulWidget {
   final bool? showIndicator;
   final List<List<ChildElement>>? childElementList;
   final TextStyle titleTextStyle;
+  final Color? selectedColor;
+  final Color? unselectedColor;
 
   const DynamicForm(this.jsonEncoded,
       {this.submitButtonAlignment,
@@ -26,13 +29,19 @@ class DynamicForm extends StatefulWidget {
       required this.finalSubmitCallBack,
       this.currentStepCallBack,
       this.formPadding = const EdgeInsets.only(left: 15, right: 15, top: 0),
-      this.formIndicatorPadding = const EdgeInsets.only(left: 10, right: 10, bottom: 15, top: 15),
-      this.titleTextStyle = const TextStyle(fontSize: 16, color: const Color(0xff222222), fontWeight: FontWeight.w500)
-      })
+      this.formIndicatorPadding =
+          const EdgeInsets.only(left: 10, right: 10, bottom: 15, top: 15),
+      this.selectedColor = const Color(0xFF3BAD59),
+      this.unselectedColor = const Color(0xFFC2C2C2),
+      this.titleTextStyle = const TextStyle(
+          fontSize: 16,
+          color: const Color(0xff222222),
+          fontWeight: FontWeight.w500)})
       : super(key: dynamicFormKey);
 
   @override
-  DynamicFormState createState() => DynamicFormState(jsonEncoded: jsonEncoded,titleTextStyle: titleTextStyle);
+  DynamicFormState createState() => DynamicFormState(
+      jsonEncoded: jsonEncoded, titleTextStyle: titleTextStyle);
 }
 
 class DynamicFormState extends State<DynamicForm> {
@@ -46,7 +55,12 @@ class DynamicFormState extends State<DynamicForm> {
   List<SingleForm> formScreen = [];
   List<String> formInformation = [];
 
-  DynamicFormState({required this.jsonEncoded,this.titleTextStyle = const TextStyle(fontSize: 16, color: const Color(0xff222222), fontWeight: FontWeight.w500)}) {
+  DynamicFormState(
+      {required this.jsonEncoded,
+      this.titleTextStyle = const TextStyle(
+          fontSize: 16,
+          color: const Color(0xff222222),
+          fontWeight: FontWeight.w500)}) {
     responseParser.setFormData = jsonEncoded;
     formScreenList = responseParser.getFormData;
     int currentIndex = -1;
@@ -55,10 +69,12 @@ class DynamicFormState extends State<DynamicForm> {
       final _formKeyNew = GlobalKey<SingleFormState>();
       formInformation.add(entry.value['title']);
       Map<String, dynamic>? filledFormData;
-      if(this.formSubmitData.isNotEmpty && this.formSubmitData.containsKey('$currentIndex')){
-        filledFormData  =  this.formSubmitData['$currentIndex'];
+      if (this.formSubmitData.isNotEmpty &&
+          this.formSubmitData.containsKey('$currentIndex')) {
+        filledFormData = this.formSubmitData['$currentIndex'];
       }
-      return SingleForm(filledFormData: filledFormData,
+      return SingleForm(
+          filledFormData: filledFormData,
           singleFormKey: _formKeyNew,
           formData: entry.value,
           titleTextStyle: titleTextStyle,
@@ -66,7 +82,9 @@ class DynamicFormState extends State<DynamicForm> {
             this.formSubmitData['$currentIndex'] = formSubmitData;
 
             widget.currentStepCallBack?.call(
-                currentIndex: currentIndex, formSubmitData: formSubmitData,isBack:false);
+                currentIndex: currentIndex,
+                formSubmitData: formSubmitData,
+                isBack: false);
             setState(() {});
           },
           finalSubmitCallBack: (index, Map<String, dynamic> formSubmitData) {
@@ -76,7 +94,7 @@ class DynamicFormState extends State<DynamicForm> {
     }).toList();
   }
 
-  void updateForm(){
+  void updateForm() {
     setState(() {
       responseParser.setFormData = widget.jsonEncoded;
       formScreenList = responseParser.getFormData;
@@ -86,17 +104,21 @@ class DynamicFormState extends State<DynamicForm> {
         final _formKeyNew = GlobalKey<SingleFormState>();
         formInformation.add(entry.value['title']);
         Map<String, dynamic>? filledFormData;
-        if(this.formSubmitData.isNotEmpty && this.formSubmitData.containsKey('$currentIndex')){
-          filledFormData  =  this.formSubmitData['$currentIndex'];
+        if (this.formSubmitData.isNotEmpty &&
+            this.formSubmitData.containsKey('$currentIndex')) {
+          filledFormData = this.formSubmitData['$currentIndex'];
         }
-        return SingleForm(filledFormData: filledFormData,
+        return SingleForm(
+            filledFormData: filledFormData,
             singleFormKey: _formKeyNew,
             formData: entry.value,
             titleTextStyle: titleTextStyle,
             nextPageButtonClick: (index, Map<String, dynamic> formSubmitData) {
               this.formSubmitData['$currentIndex'] = formSubmitData;
               widget.currentStepCallBack?.call(
-                  currentIndex: currentIndex, formSubmitData: formSubmitData,isBack:false);
+                  currentIndex: currentIndex,
+                  formSubmitData: formSubmitData,
+                  isBack: false);
               setState(() {});
             },
             finalSubmitCallBack: (index, Map<String, dynamic> formSubmitData) {
@@ -119,18 +141,22 @@ class DynamicFormState extends State<DynamicForm> {
 
     if (responseParser.getTotalFormsCount - 1 >
         responseParser.getCurrentFormNumber) {
-      if (formScreen[currentPage].singleFormKey!.currentState!.validateFields()) {
+      if (formScreen[currentPage]
+          .singleFormKey!
+          .currentState!
+          .validateFields()) {
         int currentPageIndex = responseParser.getCurrentFormNumber;
-        Map<String, dynamic> _formSubmitFinalData = responseParser.getFilledFormsData;
+        Map<String, dynamic> _formSubmitFinalData =
+            responseParser.getFilledFormsData;
 
 // Check if _formSubmitFinalData contains the key and if its value is not null or empty
         if (_formSubmitFinalData.containsKey('$currentPageIndex') &&
             _formSubmitFinalData['$currentPageIndex'] != null &&
             _formSubmitFinalData['$currentPageIndex'] is Map &&
             (_formSubmitFinalData['$currentPageIndex'] as Map).isNotEmpty) {
-
           // Safely retrieve data since we already checked it's not null and is not empty
-          Map<String, dynamic> formSubmitFinalSingleData = _formSubmitFinalData["$currentPageIndex"] as Map<String, dynamic>;
+          Map<String, dynamic> formSubmitFinalSingleData =
+              _formSubmitFinalData["$currentPageIndex"] as Map<String, dynamic>;
           formSubmitData['$currentPage'] = formSubmitFinalSingleData;
 
           Map<String, dynamic>? formInformation = formScreen[currentPage]
@@ -151,16 +177,22 @@ class DynamicFormState extends State<DynamicForm> {
                   responseParser.getCurrentFormNumber + 1;
             });
           }
-        }
-        else {
-          Map<String, dynamic>? data = formScreen[currentPage].singleFormKey!.currentState!.getFormData();
-          Map<String, dynamic>? formInformation = formScreen[currentPage].singleFormKey!.currentState!.formInformation;
+        } else {
+          Map<String, dynamic>? data = formScreen[currentPage]
+              .singleFormKey!
+              .currentState!
+              .getFormData();
+          Map<String, dynamic>? formInformation = formScreen[currentPage]
+              .singleFormKey!
+              .currentState!
+              .formInformation;
           formSubmitData['$currentPage'] = data;
-          responseParser.setFormFilledData('$currentPage',data);
+          responseParser.setFormFilledData('$currentPage', data);
           widget.currentStepCallBack?.call(
               currentIndex: currentPage + 1,
               formSubmitData: data,
-              formInformation: formInformation,isBack:false);
+              formInformation: formInformation,
+              isBack: false);
           if (data!.isNotEmpty) {
             setState(() {
               responseParser.setCurrentFormNumber =
@@ -187,7 +219,8 @@ class DynamicFormState extends State<DynamicForm> {
         widget.currentStepCallBack?.call(
             currentIndex: currentPage + 1,
             formSubmitData: data,
-            formInformation: formInformation,isBack:false);
+            formInformation: formInformation,
+            isBack: false);
         if (data!.isNotEmpty) {
           widget.finalSubmitCallBack
               ?.call(responseParser.getCurrentFormNumber, formSubmitData);
@@ -199,7 +232,6 @@ class DynamicFormState extends State<DynamicForm> {
 
   ///Preview step button click event
   void previewStepCustomClick() {
-
     Map<String, dynamic>? data = formScreen[responseParser.getCurrentFormNumber]
         .singleFormKey!
         .currentState!
@@ -210,12 +242,10 @@ class DynamicFormState extends State<DynamicForm> {
             .currentState!
             .formInformation;
 
-
     if (responseParser.getCurrentFormNumber > 0) {
       setState(() {
         responseParser.setCurrentFormNumber =
             responseParser.getCurrentFormNumber - 1;
-
       });
     }
 
@@ -223,7 +253,7 @@ class DynamicFormState extends State<DynamicForm> {
         currentIndex: responseParser.getCurrentFormNumber,
         formSubmitData: data,
         formInformation: formInformation,
-        isBack:true);
+        isBack: true);
 
     // Map<String, dynamic>? dataOld = formSubmitData["$oldPage"];//formScreen[oldPage].singleFormKey!.currentState!.getFormData();
     // Map<String, dynamic>? formInformationOld = formScreen[oldPage]
@@ -232,7 +262,6 @@ class DynamicFormState extends State<DynamicForm> {
     //     .formInformation;
 
     // formScreen[oldPage].singleFormKey!.currentState!.setFormData();
-
   }
 
   Widget? customStep(int, color, double) {
@@ -254,8 +283,8 @@ class DynamicFormState extends State<DynamicForm> {
                     size: 35,
                     padding: 8,
                     currentStep: currentPage + 1,
-                    selectedColor: selectedColor,
-                    unselectedColor: unselectedColor,
+                    selectedColor: widget.selectedColor ?? selectedColor,
+                    unselectedColor: widget.unselectedColor ?? unselectedColor,
                     onTap: (index) {
                       return () {
                         if (responseParser.getCurrentFormNumber > index) {
